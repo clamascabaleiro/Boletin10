0576ffc5af49c90dc15cc1a5baa3c98e7c81564a
clamascabaleiro <clamascabaleiro@nautilus15.danielcastelao.org>
2018-11-19 11:51:35 +0100

    Ejercicio Terminado

---
+++ Boletin10_1/src/boletin10_1/Boletin10_1.java
@@ -0,0 +1,17 @@
+
+
+package boletin10_1;
+
+/**
+ *
+ * @author clamascabaleiro
+ */
+public class Boletin10_1 {
+
+ 
+    public static void main(String[] args) {
+       Juego primerJuego = new Juego();
+       primerJuego.iniciarJuego();
+    }
+
+}
